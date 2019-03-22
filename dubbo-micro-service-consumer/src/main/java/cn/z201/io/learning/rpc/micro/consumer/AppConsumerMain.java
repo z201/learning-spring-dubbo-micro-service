b95c328c0b571e6184834f01df98c609f04dbcd1
z201 <z201_java_se@outlook.com>
2019-03-22 17:22:48 +0800

    代码进度更新

--- dubbo-micro-service-consumer/src/main/java/cn/z201/io/learning/rpc/micro/consumer/AppConsumerMain.java
+++ dubbo-micro-service-consumer/src/main/java/cn/z201/io/learning/rpc/micro/consumer/AppConsumerMain.java
@@ -1,31 +1,21 @@
 package cn.z201.io.learning.rpc.micro.consumer;
 
-import cn.z201.io.api.DefaultDemoServiceI;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author z201.coding@gmail.com
  **/
-@SpringBootApplication
+@EnableAutoConfiguration
 @Slf4j
-@ImportResource(value = {
-        "classpath:dubbo-consumer.xml"
-})
-@Controller
+@ComponentScan("cn.z201.io.learning.rpc.micro.consumer")
 public class AppConsumerMain {
 
     public static void main(String[] args) {
         System.setProperty("dubbo.application.logger","slf4j");
         SpringApplication.run(AppConsumerMain.class, args);
     }
-
-
 
 }
--- dubbo-micro-service-consumer/src/main/java/cn/z201/io/learning/rpc/micro/consumer/controller/ApiController.java
+++ dubbo-micro-service-consumer/src/main/java/cn/z201/io/learning/rpc/micro/consumer/controller/ApiController.java
@@ -1,8 +1,8 @@
-package cn.z201.io.learning.rpc.micro.consumer;
+package cn.z201.io.learning.rpc.micro.consumer.controller;
 
 import cn.z201.io.api.DefaultDemoServiceI;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class ApiController {
 
-    @Autowired
+    @Reference(version = "1.0.0")
     private DefaultDemoServiceI defaultDemoService;
 
     @RequestMapping(value = "")
-    public String runner() {
-        return defaultDemoService.sayHello("consumer");
+    public String sayHello() {
+//        return defaultDemoService.sayHello("consumer");
+        return "consumer";
     }
 }
